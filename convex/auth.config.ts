5785e44f6b1768d83a37fbfb883824173e0bdfc5
yxy <yxy_js_code@163.com>
2025-09-25 19:42:17 +0800

    修改环境变量配置

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
+      domain: 'https://organic-tapir-60.clerk.accounts.dev',
       applicationID: 'convex'
     }
   ]
